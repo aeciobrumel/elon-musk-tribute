d79c52a5e65c5f4abc98f3e658866278f383f406
Aécio Brumel <88293649+aeciobrumel@users.noreply.github.com>
2024-10-17 17:57:37 -0300

    font

---
+++ script/script.js
@@ -0,0 +1,65 @@
+window.onload = function () {
+  document.querySelector("#btn1").addEventListener("click", function () {
+    if (document.querySelector("#txt1").style.display == "none") {
+      document.querySelector("#txt1").style.display = "flex";
+    } else {
+      document.querySelector("#txt1").style.display = "none";
+    }
+  });
+
+  document.querySelector("#btn2").addEventListener("click", function () {
+    if (document.querySelector("#txt2").style.display == "none") {
+      document.querySelector("#txt2").style.display = "flex";
+    } else {
+      document.querySelector("#txt2").style.display = "none";
+    }
+  });
+
+  document.querySelector("#btn3").addEventListener("click", function () {
+    if (document.querySelector("#txt3").style.display == "none") {
+      document.querySelector("#txt3").style.display = "flex";
+    } else {
+      document.querySelector("#txt3").style.display = "none";
+    }
+  });
+
+  document.querySelector("#btn4").addEventListener("click", function () {
+    if (document.querySelector("#txt4").style.display == "none") {
+      document.querySelector("#txt4").style.display = "flex";
+    } else {
+      document.querySelector("#txt4").style.display = "none";
+    }
+  });
+
+  document.querySelector("#btn5").addEventListener("click", function () {
+    if (document.querySelector("#txt5").style.display == "none") {
+      document.querySelector("#txt5").style.display = "flex";
+    } else {
+      document.querySelector("#txt5").style.display = "none";
+    }
+  });
+
+  document.querySelector("#btn6").addEventListener("click", function () {
+    if (document.querySelector("#txt6").style.display == "none") {
+      document.querySelector("#txt6").style.display = "flex";
+    } else {
+      document.querySelector("#txt6").style.display = "none";
+    }
+  });
+
+  document.querySelector("#btn7").addEventListener("click", function () {
+    if (document.querySelector("#txt7").style.display == "none") {
+      document.querySelector("#txt7").style.display = "flex";
+    } else {
+      document.querySelector("#txt7").style.display = "none";
+    }
+  });
+
+  document.querySelector("#btn8").addEventListener("click", function () {
+    if (document.querySelector("#txt8").style.display == "none") {
+      document.querySelector("#txt8").style.display = "flex";
+    } else {
+      document.querySelector("#txt8").style.display = "none";
+    }
+  });
+};
